c6c9ad2ab4835aaf86574dac25fa69d68c2af2ad
zhaaaa7 <zhaaaaa7@gmail.com>
2018-03-06 10:39:55 -0500

    ICE6

--- burger-redux/src/axios-orders.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-//creat customized aixos instance
-const instance=axios.create({
-    baseURL:'https://react-burger7.firebaseio.com/',
-});
-
-export default instance;
--- burger-redux/src/hoc/Aux.js
+++
@@ -1,5 +0,0 @@
-//to replace <div> and wrap components
-
-const aux=(props)=>props.children;
-
-export default aux;

6bfc22bd13743f8cd757ff80f2bc8dd56aba3f7a
zhaaaa7 <zhaaaaa7@gmail.com>
2018-03-27 12:27:48 -0400

    3

--- js/js.js
+++ js/js.js
@@ -1,26 +1,53 @@
 $(document).ready(function(){
+
+  $("#menu").on("change",function(){
+    if(this.checked){
+      $("#menu-content").css( "display", "block" );
+      $(this).siblings('label').html("&#10539;"); // cross
+    }else{
+      $( "#menu-content" ).css( "display", "none" );
+      $(this).siblings('label').html("&#9776;");
+    }
+    });
+
+
   if($(window).width() <= 450){
-    $( "#menu-content" ).css( "display", "none" );
-  }else{
+    $(window).scroll(function(){
+      $( "#menu-content" ).css( "display", "none" ).fadeOut( "slow" );
+      $('label').html("&#9776;");
+      console.log('450 scroll',$(window).width());
+    });
+   }else{
     $( "#menu-content" ).css( "display", "block" );
   }
 
 
-  $(window).resize(function() {
-   if($(window).width() <= 450){
-     $( "#menu-content" ).css( "display", "none" );
-     $(window).scroll(function(){
+  $(window).scroll(function(){
+    if($(window).width() <= 450){
        $( "#menu-content" ).css( "display", "none" ).fadeOut( "slow" );
-       console.log('scroll');
-     });
-   }else{
-     $( "#menu-content" ).css( "display", "block" );
+       $('label').html("&#9776;");
+       console.log('resize scroll',$(window).width());
+      }
+    else{$( "#menu-content" ).css( "display", "block" );
+    console.log('Large screen resize scroll',$(window).width());
    }
-});
+ });
+
+  // $(window).resize(function() {
+  //   console.log('resize',$(window).width());
+  //   if($(window).width() <= 450){
+  //     $( "#menu-content" ).css( "display", "none" );
+  //     $(window).scroll(function(){
+  //      $( "#menu-content" ).css( "display", "none" ).fadeOut( "slow" );
+  //      $('label').html("&#9776;");
+  //      console.log('resize scroll',$(window).width());
+  //     });
+  //   }else{
+  //    $( "#menu-content" ).css( "display", "block" );
+  //    console.log('Large screen resize scroll',$(window).width());
+  //   }
+  // });
 
 
-    $("#menu").click(function(){
-      $("#menu-content").toggle();
-    });
 
 });

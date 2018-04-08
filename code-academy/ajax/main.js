74056ab99de648c6d324e35bf7458fa1907ae812
Shijie Zhao <zhaaaa7@gmail.com>
2018-04-08 17:19:41 -0400

    Update main.js

--- code-academy/ajax/main.js
+++ code-academy/ajax/main.js
@@ -1,61 +1,61 @@
 // Include data for accessing Google APIs
+
 const apiKey = 'AIzaSyAv4hU-zRjuCQg3j6TbDLLGuGTMa6Fza3o';
-
-const url = 'https://www.googleapis.com/urlshortener/v1/url';
-
+const url="https://www.googleapis.com/urlshortener/v1/url";
 // Some page elements
+
 const $inputField = $('#input');
 const $expandButton = $('#expand');
 const $shortenButton = $('#shorten');
 const $responseField = $('#responseField');
 
 // AJAX functions
-async function expandUrl(){
-  const urlToExpand = url + '?shortUrl=' + $inputField.val() + '&key=' + apiKey;
-  try{
-    let response=await fetch(urlToExpand);
+
+function expandUrl() {
+const urlToExpand=url + '?shortUrl=' + $inputField.val() + '&key=' + apiKey;
+  fetch(urlToExpand).then(response=>{
     if(response.ok){
-       let jsonResponse=await response.json();
-       $responseField.append('<p> Your expanded URL is </p><p>' + jsonResponse.longUrl+ '</p>');
-       return jsonResponse;
+      return response.json();
     }
-  }catch(error){
-    console.log(error);
-  }
-}
-
-async function shortenUrl(){
-  const urlToShorten = $inputField.val();
-  const urlWithKey = url + '?key=' + apiKey;
-  try{   
-    let response= await fetch(urlWithKey,{
-      method:'POST',
-      body:JSON.stringify({longUrl: urlToShorten}),
-      headers: {"Content-type": "application/json"}
-    });
-    if(response.ok){
-       let jsonResponse= await response.json();
-       $responseField.append('<p> Your shortened URL is </p><p>' + jsonResponse.id + '</p>');
-       return jsonResponse;
-       }
     throw new Error('Request failed!');
-  }catch(error){
-    console.log("[error]",error);
-  }
-}
+  },networkError=>{
+    console.log(networkError.message);
+  });
+};
 
-// Clear page and call AJAX functions
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = $inputField.val();
+  fetch(urlWithKey, {
+    method: 'POST', 
+    headers: {
+      "Content-type": "application/json"
+    }, 
+    body: JSON.stringify({longUrl: urlToShorten})
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => console.log(networkError.message)).then(jsonResponse=>{
+    $responseField.append('<p> Your shortened URL is </p><p>' + jsonResponse.id + '</p>');
+return jsonResponse;
+  });
+};
+
 function expand() {
   $responseField.empty();
   expandUrl();
   return false;
-};
+}
 
 function shorten() {
   $responseField.empty();
   shortenUrl();
   return false;
-};
+}
+
+// Call functions on submit
 
 $expandButton.click(expand);
 $shortenButton.click(shorten);
